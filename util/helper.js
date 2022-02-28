8dad20f0adfdf33c7ab2c4390a0f047bf000fab5
ljubisav89 <ljubisav89@hotmail.com>
2022-02-28 15:58:56 +0100

    de

--- util/helper.js
+++
@@ -1,9 +0,0 @@
-const createRoom = async function (client, name) {
-    try {
-        const newCollection = await client.createCollection(name);
-        return newCollection
-    } catch (e) { return e.response.body }
-
-}
-
-module.exports = { createRoom, }
